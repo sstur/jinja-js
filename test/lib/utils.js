a8898b90915f135ecbc1b15f3bf8853455b140b9
Simon Sturmer <sstur@me.com>
2013-02-06 15:03:14 +0000

    add test/lib

---
+++ test/lib/utils.js
@@ -0,0 +1,39 @@
+/*global global, require, exports */
+(function() {
+  "use strict";
+  var fs = require('fs');
+
+  var fileCache = global.fileCache || {};
+
+  var jinja = require('jinja');
+
+  jinja.readTemplateFile = getTemplate;
+
+  function getTemplate(name) {
+    var file = (~name.indexOf('.')) ? name : name + '.html';
+    file = './views/' + file;
+    if (file in fileCache) {
+      return fileCache[file];
+    }
+    //todo: read files at module load time
+    return fileCache[file] = fs.readFileSync(file, 'utf8');
+  }
+
+  var filters = {
+    split: function(str, sep) {
+      return ('' + str).split(sep || '');
+    }
+  };
+
+  exports.render = function(name, context, opts) {
+    context = context || {};
+    opts = opts || {};
+    var text = getTemplate(name);
+    var rendered = jinja.render(text, context, {filters: filters});
+    if (opts.trim !== false) {
+      rendered = rendered.trim();
+    }
+    return rendered;
+  };
+
+})();
