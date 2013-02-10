85adf2161bf639c1ab775b450f42d6ff97540173
Simon Sturmer <sstur@me.com>
2013-02-10 15:03:58 +0000

    add analytics

--- demo/demo.js
+++ demo/demo.js
@@ -69,3 +69,11 @@ jQuery(function($) {
   }
 
 });
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-35213673-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
