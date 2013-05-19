ba677f434846f4b37700f266a47b1dd26ae13214
Simon Sturmer <sstur@me.com>
2013-05-19 17:50:33 +0000

    add version and link to comments

--- jinja.js
+++ jinja.js
@@ -3,6 +3,7 @@
  * https://github.com/sstur/jinja
  *
  * This is a slimmed-down Jinja2 implementation [http://jinja.pocoo.org/]
+ *
  * In the interest of simplicity, it deviates from Jinja2 as follows:
  * - Line statements, cycle, super, macros and block nesting are not implemented
  * - auto escapes html by default (the filter is "html" not "e")
